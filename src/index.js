45e226741248aceea8a302b6ff81ee9923dff729
Rafhael Gomes <rafhael@protonmail.com>
2022-05-23 13:24:00 -0300

    feat: add express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-const express = require("express");
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 3333
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`)
+})
