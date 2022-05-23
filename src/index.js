b88e07ec4b5cc77b5a836070f56b9859954148df
Rafhael Gomes <rafhael@protonmail.com>
2022-05-23 13:32:03 -0300

    feat: add POST on /account route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 require('dotenv').config()
 const express = require('express')
+const crypto = require('crypto')
 
 const app = express()
+const customers = []
 
 app.use(express.json())
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body
+
+  const id = crypto.randomUUID()
+
+  const newAccount = { cpf, name, id, statement: [] }
+
+  customers.push(newAccount)
+
+  return res.status(201).end()
+})
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
